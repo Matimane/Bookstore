402f8a087515a1867cc8334e0e4b655bb4d50a77
Matimane <1matias.sainio@gmail.com>
2025-09-15 00:02:03 +0300

    booklist

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,5 +1,7 @@
 package com.example.bookstore.web;
 
+import com.example.bookstore.domain.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BookController {
 
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @GetMapping("/index")
     public String showIndex(Model model) {
-
+        model.addAttribute("books", bookRepository.findAll());
         return "index";
     }
 }
